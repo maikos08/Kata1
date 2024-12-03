67cd438dd60a49b44ee0a6fc6388c0d3ec89a693
Miguel Ángel <migue.rodrii08@gmail.com>
2024-12-03 11:54:03 +0000

    October 13th push was not executed correctly

--- src/software/ulpgc/Main.java
+++ src/software/ulpgc/Main.java
@@ -1,4 +1,10 @@
 package software.ulpgc;
 
-public class Main {
+import java.time.LocalDate;
+
+public class Main
+{
+    public static void main(String[] args) {
+        System.out.println(new Person("Michael", LocalDate.of(2004,8,27)));
+    }
 }
--- src/software/ulpgc/Person.java
+++ src/software/ulpgc/Person.java
@@ -1,4 +1,40 @@
 package software.ulpgc;
 
-public class Person {
+import java.time.LocalDate;
+
+public class Person
+{
+    public static final int Days_Per_Year = 365;
+    private final String name;
+    private final LocalDate birthdate;
+
+    public Person(String name, LocalDate birthdate) {
+        this.name = name;
+        this.birthdate = birthdate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public int getAge() {
+        return toYears(LocalDate.now().toEpochDay() - birthdate.toEpochDay());
+    }
+
+    private int toYears(long days) {
+        return (int) (days / Days_Per_Year);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", birthdate=" + birthdate +
+                ", age=" + getAge() +
+                '}';
+    }
 }
